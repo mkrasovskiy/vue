4dc6cd2fed47f5db6a0714aa048c89d08b8c937d
Максим Красовский <maksimkrasovskiy@Air-Maksim.Dlink>
2024-02-12 18:43:58 +0300

    Добавил контент на страницу Home

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,72 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/blog">Blog</router-link> |
-    <router-link to="/blogdetails">Blog Details</router-link> |
-    <router-link to="/error">Error</router-link>
-  </nav>
+  <header class="header center">
+    <router-link class="header__logo" to="/">
+      <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z" fill="#CDA274"/>
+      </svg>                
+      <h1 class="header__logo_title">Interno</h1>
+    </router-link>
+    <nav class="header__nav">
+      <router-link class="header__menu-link" to="/">Home</router-link>
+      <router-link class="header__menu-link" to="/blog">Blog</router-link>
+      <router-link class="header__menu-link" to="/blogdetails">Blog Details</router-link>
+      <router-link class="header__menu-link" to="/error">Error</router-link>
+    </nav>
+  </header>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'DM Serif Display', serif;
-  font-family: 'Jost', sans-serif;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
+  #app {
+    margin: 0;
+    padding: 0;
+    font-family: 'Jost', sans-serif;
+    font-weight: 400;
+    color: #4D5053;
+  }
+  h1 {
+    font-family: 'DM Serif Display', serif;
+    font-weight: 400;
+  }
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    text-decoration: none;
+    cursor: pointer;
+    color: #4D5053;
+  }
+  ul {
+    list-style: none;
+  }
+  .center {
+    max-width: 1200px;
+    width: 100%;
+    margin: 0 auto;
+  }
+  .header {
+    margin: 44px auto 45px auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &__logo {
+      display: flex;
+      align-items: center;
+      gap: 10px;
+      &_title {
+          font-size: 40px;
+          line-height: 50px;
+      }
+    }
+    &__nav {
+      width: 300px;
+      display: flex;
+      justify-content: space-between;
+    }
+    &__menu-link {
+      font-size: 20px;
+      line-height: 25px;
+    }
+    &__menu-link:hover {
+      opacity: 0.7;
+      transition: .3s;
     }
   }
-}
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="home">
-    <h1>This is an home page</h1>
-  </div>
+  <HomeBanner/>
+  <HomeProject/>
+  <HomeCounter/>
+  <HomeBlog/>
+  <FooterComp/>
 </template>
+
+<script>
+  import HomeBanner from '@/components/HomeBanner.vue';
+  import HomeProject from '@/components/HomeProject.vue';
+  import HomeCounter from '@/components/HomeCounter.vue';
+  import HomeBlog from '@/components/HomeBlog.vue';
+  import FooterComp from '@/components/FooterComp.vue';
+
+  export default {
+    name: 'HomePage',
+    components: {
+      HomeBanner,
+      HomeProject,
+      HomeCounter,
+      HomeBlog,
+      FooterComp
+    },
+    data() {
+        return {};
+    },
+    mounted() {
+    },
+    methods: {},
+    
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
